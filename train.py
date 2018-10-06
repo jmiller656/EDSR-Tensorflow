eb31775a7dbbb2064cf64d85c2bb0912a92f4028
Josh Miller <jxm7374@rit.edu>
2018-10-06 14:17:31 -0400

    Add warning for mismatched image size and scale

--- train.py
+++ train.py
@@ -12,6 +12,9 @@ parser.add_argument("--savedir",default='saved_models')
 parser.add_argument("--iterations",default=1000,type=int)
 args = parser.parse_args()
 data.load_dataset(args.dataset,args.imgsize)
+if args.imgsize % args.scale != 0:
+    print(f"Image size {args.imgsize} is not evenly divisible by scale {arg.scale}")
+    return
 down_size = args.imgsize//args.scale
 network = EDSR(down_size,args.layers,args.featuresize,args.scale)
 network.set_data_fn(data.get_batch,(args.batchsize,args.imgsize,down_size),data.get_test_set,(args.imgsize,down_size))
